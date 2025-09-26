f360c082f2b7e5242e6e12375c2ad7a4eecdae88
Civantic <james.kaminski@civantic.com>
2025-09-26 09:15:04 -0600

    Update next.config.js

--- apps/outbreakresponse/next.config.js
+++ apps/outbreakresponse/next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
-  // TEMP: keep this while we finish stabilizing TS build
-  typescript: { ignoreBuildErrors: true }
+  reactStrictMode: true
 }
-
 module.exports = nextConfig
